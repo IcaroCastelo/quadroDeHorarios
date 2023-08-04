8b1b01115d91c4011b7fabd2e49bf922c6b5c668
atitudedeveloper <atitudedeveloper@gmail.com>
2023-08-04 14:14:21 -0300

    ajustes no mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,9 +129,7 @@
                     { "width": "15%", "targets": 2 },
                     { "width": "15%", "targets": 3 },
                     { "width": "10%", "targets": 4 },
-                    // { "width": "25%", "targets": 5 },
                 ],
-                // scrollX: true,
                 columns: [
                     { data: 'DIASEMANA' },
                     { data: 'HORARIO'},

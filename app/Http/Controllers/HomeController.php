199c81bc9d881061478b15adc8e571b1b6d43a5d
atitudedeveloper <atitudedeveloper@gmail.com>
2025-02-06 07:12:29 -0300

    correcoes de detalhes finais

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,73 +18,42 @@ class HomeController extends Controller
         $cpf =  $request->cpf;
   
         $quadroDeHorario = DB::select("SELECT 
-        UNIDADE, 
-        SALA, 
-        DIASEMANA,
-        NUMEROSEMANA,
-        REPLACE(UNIDADE, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS UNIDADE,
-	REPLACE(SALA, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS SALA, 
-	REPLACE(DIASEMANA, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS DIASEMANA,	
-	REPLACE(CONCAT(HORAINICIAL, ' - ', HORAFINAL), '- - -', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS HORARIO,
-        DISCIPLINA,
-        CURSO,
-        MATRICULA,
-        ALUNO,
-        CPF,
-        DESCBLOCO
-    FROM
-    Corpore.dbo.VW_COMPROVANTE_MATRICULA_PORTAL_QUADRO_HORARIO(NOLOCK)
-    WHERE
-        CPF = '$cpf'--:RA1
-    AND CODCOLIGADA = 1--:CODCOLIGADA1
-    -- AND  [PERIODO LETIVO] = 20242
-    ORDER BY
-        NUMEROSEMANA
-        ");
+            UNIDADE, 
+            SALA, 
+            DIASEMANA,
+            NUMEROSEMANA,
+            REPLACE(UNIDADE, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS UNIDADE,
+        REPLACE(SALA, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS SALA, 
+        REPLACE(DIASEMANA, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS DIASEMANA,	
+        REPLACE(CONCAT(HORAINICIAL, ' - ', HORAFINAL), '- - -', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS HORARIO,
+            DISCIPLINA,
+            CURSO,
+            MATRICULA,
+            ALUNO,
+            CPF,
+            DESCBLOCO
+        FROM
+        Corpore.dbo.VW_COMPROVANTE_MATRICULA_PORTAL_QUADRO_HORARIO(NOLOCK)
+        WHERE
+            CPF = '$cpf'--:RA1
+        AND CODCOLIGADA = 1--:CODCOLIGADA1
+        -- AND  [PERIODO LETIVO] = 20242
+        ORDER BY
+            NUMEROSEMANA
+            ");
 
-    //    $retorno['data'] = $quadroDeHorario;
+        $horariosAgrupados = [];
+        foreach ($quadroDeHorario as $item) {
+            $dia = $item->DIASEMANA;
+            // dd($dia);
+            if (!isset($horariosAgrupados[$dia])) {
+                $horariosAgrupados[$dia] = [];
+            }
+            $horariosAgrupados[$dia][] = $item;
+        }
 
-    //    return $retorno;
-    //     $cpf =  $request->cpf;
-  
-    //     $quadroDeHorario = DB::select("SELECT 
-    //     UNIDADE, 
-    //     SALA, 
-    //     DIASEMANA,
-    //     NUMEROSEMANA,
-    //     REPLACE(UNIDADE, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS UNIDADE,
-	// REPLACE(SALA, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS SALA, 
-	// REPLACE(DIASEMANA, '-', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS DIASEMANA,	
-	// REPLACE(CONCAT(HORAINICIAL, ' - ', HORAFINAL), '- - -', CASE WHEN DISCIPLINA LIKE '%- Distância' THEN 'EAD' ELSE 'PRESENCIAL' END) AS HORARIO,
-    //     DISCIPLINA,
-    //     CURSO,
-    //     MATRICULA,
-    //     ALUNO,
-    //     CPF,
-    //     DESCBLOCO
-    // FROM
-    //     Corpore.dbo.VW_COMPROVANTE_MATRICULA_PORTAL_QUADRO_HORARIO(NOLOCK)
-    // WHERE
-    //     CPF = '$cpf'--:RA1
-    // AND CODCOLIGADA = 1--:CODCOLIGADA1
-    // ORDER BY
-    //     NUMEROSEMANA
-    //     ");
+        return response()->json(['data' => $horariosAgrupados]);
 
-$horariosAgrupados = [];
-foreach ($quadroDeHorario as $item) {
-    $dia = $item->DIASEMANA;
-    // dd($dia);
-    if (!isset($horariosAgrupados[$dia])) {
-        $horariosAgrupados[$dia] = [];
-    }
-    $horariosAgrupados[$dia][] = $item;
-}
-
-return response()->json(['data' => $horariosAgrupados]);
-
-    //    $retorno['data'] = $quadroDeHorario;
-
-    //    return $retorno;
+   
     }
 }
